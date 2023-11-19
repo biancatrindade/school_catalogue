9fdedc2c9a60cfe418d2398602639103893098af
biancatrindade <biancatrindade.mkt@gmail.com>
2023-11-19 16:37:35 +0000

    Uploading the initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Project: School Catalogue
+
+Hello!
+
+I'm Bianca, and this project is part of the Codecademy course **"Learn Intermediate JavaScript."**
+
+I created a digital school catalog that holds quick reference material for each school in the city. The parent and three child classes have the following properties, getters, setters, and methods:
+
+### School - Parent class
+- Properties: `name` (string), `level` (one of three strings: `'primary'`, `'middle'`, or `'high'`), and `numberOfStudents` (number)
+- Getters: All properties have getters
+- Setters: The `numberOfStudents` property has a setter
+- Methods: `.quickFacts()` and `.pickSubstituteTeacher()` (this is a static method)
+
+### Primary
+- Includes everything in the `School` class, plus one additional property
+- Properties: `pickupPolicy` (string)
+
+### Middle
+- Does not include any additional properties or methods
+
+### High
+- Includes everything in the `School` class, plus one additional property
+- Properties: `sportsTeams` (array of strings)
+
+
+You can check the result in [`school_catalogue.js`](https://github.com/biancatrindade/build-a-library/blob/main/build_a_library.js).
+
+**See you there!**
---
+++ school_catalogue.js
@@ -0,0 +1,63 @@
+class School {
+    constructor(name, level, numberOfStudents) {
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get level() {
+        return this._level;
+    }
+
+    get numberOfStudents() {
+        return this._numberOfStudents;
+    }
+
+    set numberOfStudents(newNumberOfStudents) {
+        return typeof newNumberOfStudents === 'number' ? this._numberOfStudents : console.log('Invalid input: numberOfStudents must be set to a Number.');
+    }
+
+    quickFacts() {
+        console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`)
+    }
+
+    static pickSubstituteTeacher(substituteTeachers) {
+        const randomTeacher = Math.floor(Math.random()*substituteTeachers.length);
+        return substituteTeachers[randomTeacher];
+    }
+}
+
+class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickupPolicy) {
+        super(name, 'primary', numberOfStudents);
+        this._pickupPolicy = pickupPolicy;
+    }
+
+    get pickupPolicy() {
+        return this._pickupPolicy;
+    }
+}
+
+class MiddleSchool extends School {
+    constructor(name, numberOfStudents) {
+        super(name, 'middle', numberOfStudents);
+    }
+}
+
+class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+        super(name, 'high', numberOfStudents);
+        this._sportsTeams = sportsTeams;
+    }
+
+    get sportsTeams() {
+        console.log(this._sportsTeams); 
+    }
+
+}
+
+//Testing if the code is working
